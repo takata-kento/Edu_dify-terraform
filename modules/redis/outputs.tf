fff721898c6abc26f1f310be547f7cd5c8092b91
takata-kento <takata_kento@ims-sol.co.jp>
2025-03-31 09:19:42 +0900

    fix: create redis output of endpoint address and port number

---
+++ modules/redis/outputs.tf
@@ -0,0 +1,7 @@
+output "primary_endpoint_address" {
+  value = aws_elasticache_replication_group.main.primary_endpoint_address
+}
+
+output "port" {
+  value = aws_elasticache_replication_group.main.port
+}
